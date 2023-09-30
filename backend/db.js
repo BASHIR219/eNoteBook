5e6e1e1896e48bbe6bd21c61c0d2eb9cca4fbd6a
BASHIR219 <97796347+BASHIR219@users.noreply.github.com>
2023-09-30 16:42:22 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://Bashir00219:Bashir%40321@cluster0.qdp9bbt.mongodb.net/your_database?retryWrites=true&w=majority";
+
+const mongoURI =
+  "mongodb+srv://Bashir00219:Bashir%40321@cluster0.qdp9bbt.mongodb.net/your_database?retryWrites=true&w=majority";
 
 // Function to connect with MongoDB
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB  successfully");
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to MongoDB successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
+    // Exit the process if MongoDB connection fails
+    process.exit(1);
   }
 };
 

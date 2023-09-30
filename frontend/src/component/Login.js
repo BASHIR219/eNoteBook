fbd669d2bd767acadde2c813f525029ee4ab5dea
BASHIR219 <97796347+BASHIR219@users.noreply.github.com>
2023-09-30 12:57:11 +0530

    Update Login.js

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -8,7 +8,7 @@ export default function Login(props) {
   
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("http://e-note-book-backend-sepia.vercel.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

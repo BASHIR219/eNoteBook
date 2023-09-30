4e15f533d9f14dc8a709d3974deaa05140b7862f
BASHIR219 <97796347+BASHIR219@users.noreply.github.com>
2023-09-30 16:33:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,9 @@ app.use(cors(
 ));
 app.use(express.json());
 
+// Handle preflight requests for all routes
+app.options('*', cors());
+
 //availabe routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
